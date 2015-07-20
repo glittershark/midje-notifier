2ba4441d26a6e586d71f97d45c4944a521dae8ea
blak3mill3r <blak3mill3r@gmail.com>
2015-07-20 11:26:53 -0700

    refactor and add color to notify-send
    the background color indicates whether all tests passed or not
    
    refactored:
      replace macros with functions
      use a multimethod to allow easy additions of other notification tools
      add a default (print to stdout) in case no supported tool is available

--- src/midje/notifier.clj
+++ src/midje/notifier.clj
@@ -2,28 +2,47 @@
   (:require [robert.hooke :refer [with-scope append]]
             [clojure.java.shell :refer [sh]]))
 
-(defmacro exists? [program] `(= 0 (:exit (sh "which" ~program))))
+(defn exists? [program]
+  (->> program
+       (sh "which")
+       :exit
+       (= 0)))
 
 (defonce notification-type
   (cond
     (exists? "notify-send")       :notify-send
     (exists? "terminal-notifier") :terminal-notifier))
 
-(defmacro notify-send [title body]
-  (case notification-type
-    :notify-send       `(sh "notify-send" ~title ~body)
-    :terminal-notifier `(sh ":terminal-notifier" "-message" ~body "-title" ~title)))
+(defmulti send-notification (constantly notification-type))
+
+(defmethod send-notification :notify-send
+  [title body & {:keys [pass?]}]
+  (sh "notify-send"
+      "-h" (str "string:bgcolor:"(if pass? "#008800" "#ff4444"))
+      title body))
+
+(defmethod send-notification :terminal-notifier
+  [title body & {:keys [pass?]}]
+  ;; FIXME pass? is ignored, some iPerson please add some visual indicator of pass/fail
+  (sh "terminal-notifier" "-message" body "-title" title))
+
+(defmethod send-notification :default
+  [title body & {:keys [pass?]}]
+  (println "Unable to send notification, do you have either terminal-notifier or notify-send on your $PATH?")
+  (println title body pass?))
 
 (defn send-notifications []
   (let [{passes :midje-passes failures :midje-failures}
         (midje.emission.state/output-counters)]
-    (notify-send
-      "Tests complete"
-      (if (= 0 failures)
-        (str "All checks (" passes ") succeeded.")
-        (str failures " checks failed, " passes " succeeded")))))
+    (let [passed (= 0 failures)]
+      (send-notification
+       "Tests complete"
+       (if passed
+         (str "All checks (" passes ") succeeded.")
+         (str failures " checks failed, " passes " succeeded"))
+       :pass? passed))))
 
-(defmacro notify-me
-  "Send notifications using notify-send about all successive Midje test results"
+(defn notify-me
+  "Send notifications using notify-send or terminal-notifier after each Midje test run"
   []
-  `(append midje.emission.api/fact-stream-summary (send-notifications)))
+  (append midje.emission.api/fact-stream-summary (send-notifications)))
